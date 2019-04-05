bd6ea3f4cd4171f3ab8ca41b8ea694855a1dd678
farhan <farhanfazrian@gmail.com>
2019-04-05 15:22:13 +0700

    add custom banner

---
+++ src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package guru.springframework.joke.controllers;
+
+import guru.springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"","/"})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "chucknorris";
+    }
+}
